432af9914dc53a3f121aa28d7e634036cf4a1c2e
A-b-h-i-s-h-e-k-Github-K-u-m-a-r <77101044+A-b-h-i-s-h-e-k-Github-K-u-m-a-r@users.noreply.github.com>
2022-04-15 11:33:34 +0530

    Update __init__.py

--- src/ruptures/detection/__init__.py
+++ src/ruptures/detection/__init__.py
@@ -6,3 +6,8 @@ from .dynp import Dynp
 from .kernelcpd import KernelCPD
 from .pelt import Pelt
 from .window import Window
+
+from .dynpensembling import DynpEnsembling
+from .windowensembling import WindowEnsembling
+from .binsegensembling import BinsegEnsembling
+from .bottomupensembling import BottomUpEnsembling
